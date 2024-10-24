e58ad1876613b2d3b4243d19564f787be7dfbc9e
danielsich <daniel.sich@uni-a.de>
2024-10-24 19:02:57 +0200

    Created coordinates
    100 Coordinates with random distribution with x and y between 0 and 100

---
+++ coordinates.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 24 18:52:25 2024
+
+@author: Daniel
+"""
+
+import numpy as np
+coordinates = np.random.uniform(0,100, size =(100,2) )
+
+print(coordinates)
