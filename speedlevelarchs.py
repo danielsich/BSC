70b6e9fc08fddd496c1a011d9a84b0d3ae4ed69e
danielsich <daniel.sich@uni-a.de>
2024-10-29 14:26:23 +0100

    Update speedlevelarchs.py
    get the speedlevels as an output,
    match it with the solver file as it is a decision variable

--- speedlevelarchs.py
+++ speedlevelarchs.py
@@ -12,13 +12,17 @@ np.random.seed(37)
 
 ## load archs
 Archs = np.load('Archs.npy') ## relevant Archs
-
+##print(Archs)
 ##define Vr
 def levels(low, high, level):
     diff = high - low
     ranges = diff/level
     lvl = [(low + i * ranges + 0.5 * ranges) for i in range(level)]
+    np.save('lvl.npy',lvl)
     return np.array(lvl)
+
+a = levels(40, 100, 60)
+
 '''
 def speedarchs(low, high, level, Arch):
     outp = for i in range(len(Arch)):
