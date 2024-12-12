b022688a0af242b5ee3f08f135387a7232ad4797
sichdani <daniel.sich@uni-a.de>
2024-12-12 19:01:17 +0100

    add more relevantcalculations to multi

--- solvermulti.py
+++ solvermulti.py
@@ -21,9 +21,24 @@ def tupls (a):
     arcs = [(i, j) for i in range(a) for j in range(a) if i != j]
     return np.array(arcs)
 
-# calc for relevant archs
-def relevantcustomers(a,N):
-    ##define used customers for calculation
+# string relevant customers
+def relevantcusta(a):
     inp = np.random.choice(np.arange(1, N.shape[0]), size=a, replace=False)
     inp = np.insert(inp, 0, 0)
+    return inp
+# calc for relevant customers
+def relevantcustomers(inp,N):
     relN = N[0, :]
+    ##np.save('relN',inp)
+    i = 0
+    j = 0
+    for i in range(len(inp)):
+        j = inp[i]
+        if (j != 0):
+            relN = np.vstack((relN, N[j, :]))
+    return relN
+print(relevantcusta(5))
+#define relvant Distnces
+def relevantdistances(relevantcusta,Distall):
+    return
+
