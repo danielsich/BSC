5671fccb5b8535a17d116d93b8a12e29a0d065d4
sichdani <daniel.sich@uni-a.de>
2025-01-17 17:41:00 +0100

    overview median idea

---
+++ Data_Analysis/mediantts.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Load datasets
+file_paths = {
+    'outpdserver': '../output/outpdsizeserver.csv',
+    'outplserver': '../output/outplserver.csv',
+    'outpeserver': '../output/outpeserver.csv',
+    'outprpserver': '../output/outprpsize10.csv'
+}
+
+quartiles_dict = {}
+
+for name, file_path in file_paths.items():
+    df = pd.read_csv(file_path, na_values='nan')
+
+    # Convert tts to numeric
+    df['tts'] = pd.to_numeric(df['tts'], errors='coerce')
+
+    # Fill NaN in tts column with 600
+    df['tts'].fillna(600, inplace=True)
+
+    # Calculate quartiles
+    quartiles = df.groupby('customers')['tts'].median()
+    quartiles_dict[name] = quartiles
+
+# Plotting
+plt.figure(figsize=(12, 6))
+
+for name, quartiles in quartiles_dict.items():
+    plt.plot(quartiles.index, quartiles, label=f'{name} Median')
+
+plt.yscale('log')
+plt.xlabel('Kunden', fontsize=14)
+plt.ylabel('Rechenzeit', fontsize=14)
+plt.xticks(fontsize=14)
+plt.yticks(fontsize=14)
+plt.legend(fontsize=14)
+plt.grid(True)
+#plt.title('Median tts per Customersize for Different Datasets', fontsize=16)
+#plt.savefig('../vis/tts_medians_comparison.svg', format='svg')
+plt.show()
