36b13fe2f67d954d65df3d8aa3bfc0acf67863b5
sichdani <daniel.sich@uni-a.de>
2025-01-29 17:09:35 +0100

    calc totalcost per 100 km

--- time/mod.py
+++ time/mod.py
@@ -4,17 +4,17 @@ import pandas as pd
 file_path = '../time/out.csv'
 df = pd.read_csv(file_path)
 
-# Filter out rows where 'customers' has a value and all other columns except 'timewindows' are NaN
+# Filter out rows where nan vallues exist
 filtered_df = df.dropna()
 
-# Calculate 'fuel per 100 000 distance' for each line
+# Calculate 'fuel per 100 km' for each line
 filtered_df['fuel_per_100km_distance'] = (filtered_df['total_fuel'] / filtered_df['distance']) * 100000
 
-# Calculate 'totalcost' for each line
-#filtered_df['totalcost'] = filtered_df['costs'] + filtered_df['driver_pay']
-
-# Calculate 'drivercost per 100 000 distance' for each line
+# Calculate 'drivercost per 100 km' for each line
 filtered_df['drivercost_per_100km_distance'] = (filtered_df['driver_pay'] / filtered_df['distance']) * 100000
+
+# Calculate 'drivercost per 100 km' for each line
+filtered_df['totalcost_per_100km_distance'] = (filtered_df['costs'] / filtered_df['distance']) * 100000
 
 # Save the final DataFrame to 'outmod.csv'
 filtered_df.to_csv('../time/outmod.csv', index=False)
