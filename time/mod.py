260f78803a31af546ae27d93ee0d2f5b68b3d86a
Daniel Sich <105250341+danielsich@users.noreply.github.com>
2025-01-29 14:29:04 +0100

    modify out.csv

--- time/mod.py
+++ time/mod.py
@@ -8,13 +8,13 @@ df = pd.read_csv(file_path)
 filtered_df = df.dropna()
 
 # Calculate 'fuel per 100 000 distance' for each line
-filtered_df['fuel_per_100000_distance'] = (filtered_df['total_fuel'] / filtered_df['distance']) * 100000
+filtered_df['fuel_per_100km_distance'] = (filtered_df['total_fuel'] / filtered_df['distance']) * 100000
 
 # Calculate 'totalcost' for each line
 #filtered_df['totalcost'] = filtered_df['costs'] + filtered_df['driver_pay']
 
 # Calculate 'drivercost per 100 000 distance' for each line
-filtered_df['drivercost_per_100000_distance'] = (filtered_df['driver_pay'] / filtered_df['distance']) * 100000
+filtered_df['drivercost_per_100km_distance'] = (filtered_df['driver_pay'] / filtered_df['distance']) * 100000
 
 # Save the final DataFrame to 'outmod.csv'
 filtered_df.to_csv('../time/outmod.csv', index=False)
