6fe3aa722123de22061c8ed2902af2de2fe9cb75
Daniel Sich <105250341+danielsich@users.noreply.github.com>
2025-01-29 13:46:30 +0100

    modify vis

--- Data_Analysis/ttsquartiles.py
+++ Data_Analysis/ttsquartiles.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # csv2Dataframe
-file_path = '../output/outpeserver.csv'
+file_path = '../output/outprpsize10.csv'
 df = pd.read_csv(file_path, na_values='nan')
 
 
@@ -33,5 +33,5 @@ plt.yticks(fontsize=14)
 #plt.title('25%, 50%, und 75% Quartil je Kundenanzahl für das PRP')
 plt.legend(fontsize = 14)
 plt.grid(True)
-plt.savefig('../vis/tts_quartilespe.svg', format='svg')
+plt.savefig('../vis/tts_quartilesprp.svg', format='svg')
 plt.show()
