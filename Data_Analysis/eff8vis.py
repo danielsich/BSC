3fdb195deedd02c03e1dd3e60803d77a0592bb94
sichdani <daniel.sich@uni-a.de>
2025-02-04 18:39:33 +0100

    move

--- Data_Analysis/eff8vis.py
+++ Data_Analysis/eff8vis.py
@@ -8,19 +8,29 @@ df = pd.read_csv(file_path)
 # Group by the 'eff' column to calculate the average values
 eff_averages = df.groupby('eff').mean()
 
+# Adjust the distances and calculate fuel consumption per 100 km
+eff_averages['distance_km'] = eff_averages['distance'] / 1000
+eff_averages['fuel_per_100km'] = (eff_averages['total_fuel'] / eff_averages['distance_km']) * 100
+
 # Plotting
-plt.figure(figsize=(12, 6))
+fig, ax1 = plt.subplots(figsize=(12, 6))
 
-plt.plot(eff_averages.index, eff_averages['costs'], color='blue', label='Kosten')
+color = 'tab:blue'
+ax1.set_xlabel('ε', fontsize=14)
+ax1.set_ylabel('€', color=color, fontsize=14)
+ax1.plot(eff_averages.index, eff_averages['costs'], color=color, label='Kosten')
+ax1.tick_params(axis='y', labelcolor=color)
 
-plt.xlabel('ε', fontsize=14)
-plt.ylabel('€', fontsize=14)
-#plt.title('Effizienz vs Kosten', fontsize=16)
-plt.legend()
-plt.grid(True)
+ax2 = ax1.twinx()  # Instantiate a second axes that shares the same x-axis
+color = 'tab:green'
+ax2.set_ylabel('(Liter/100 km)', color=color, fontsize=14)  # We already handled the x-label with ax1
+ax2.plot(eff_averages.index, eff_averages['fuel_per_100km'], color=color, linestyle='dashed', label='Kraftstoffverbrauch')
+ax2.tick_params(axis='y', labelcolor=color)
 
-# Save the plot
-#plt.savefig('../vis/eff_vs_costs50.svg', format='svg')
+fig.tight_layout()  # Otherwise, the right y-label is slightly clipped
+#plt.title('Effizienz vs Gefahrene Distanz und Kraftstoffverbrauch', fontsize=16)
+fig.legend(loc='upper left', bbox_to_anchor=(0.1, 0.9))
+plt.savefig('../vis/eff_vs_costs50.svg', format='svg')
 
 # Show the plot
 plt.show()
