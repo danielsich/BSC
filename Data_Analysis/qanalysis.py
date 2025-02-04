4b95a2fb26cd7a5a3a7760cc68e01a8de260b039
sichdani <daniel.sich@uni-a.de>
2025-02-04 21:29:18 +0100

    vis q analysis

--- Data_Analysis/qanalysis.py
+++ Data_Analysis/qanalysis.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load the dataset
-file_path = '../qi/qi20.csv'
+file_path = '../qi/q8.csv'
 df = pd.read_csv(file_path)
 
 # Calculate the customerdemands in kilograms
@@ -17,12 +17,17 @@ fig, ax1 = plt.subplots(figsize=(12, 6))
 color = 'tab:blue'
 ax1.set_xlabel('kg', fontsize=14)
 ax1.set_ylabel('Fahrzeuge', color=color, fontsize=14)
-ax1.plot(demand_averages.index, demand_averages['vehicles'], color=color, label='Fahrzeuge')
+ax1.plot(demand_averages.index, demand_averages['vehicles'], color=color, label='Durchschnittliche Anzahl der Fahrzeuge')
 ax1.tick_params(axis='y', labelcolor=color)
 
+ax2 = ax1.twinx()  # Instantiate a second axes that shares the same x-axis
+color = 'tab:green'
+ax2.set_ylabel('km/h', color=color, fontsize=14)  # We already handled the x-label with ax1
+ax2.plot(demand_averages.index, demand_averages['averagespeed'], color=color, linestyle='dashed', label='Durchschnittsgeschwindigkeit')
+ax2.tick_params(axis='y', labelcolor=color)
+
 fig.tight_layout()  # Otherwise, the right y-label is slightly clipped
-#plt.title('Kundennachfrage vs Durchschnittliche Anzahl der Fahrzeuge', fontsize=16)
 fig.legend(loc='upper left', bbox_to_anchor=(0.1, 0.9))
 
-#plt.savefig('../vis/demand_vs_vehicles_used.svg', format='svg')
+#plt.savefig('../vis/demand_vs_vehicles_and_speed8.svg', format='svg')
 plt.show()
